61ad62484da63a25a63c89802bdf64b3cc8d7134
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2024-08-09 20:46:11 -0300

    insersão de novo style

--- lib/main.dart
+++ lib/main.dart
@@ -15,3 +15,33 @@ class Sextou extends StatelessWidget {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const Sextou());
+// }
+
+// class Sextou extends StatelessWidget {
+//   const Sextou({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       title: 'Balada Segura',
+//       home: Scaffold(
+//         body: Center(
+//           child: Text(
+//             'Sextou!',
+//             style: TextStyle(
+//               color: Colors.blue, // Define a cor do texto
+//               decoration: TextDecoration.none, // Remove o sublinhado
+//               fontSize: 24, // Opcional: Define o tamanho da fonte
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
